6fc8aa432d901ff92b2a811a0f66cbdb63177215
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2025-10-28 11:05:48 -0700

    add is_valid_syntax

--- ruff_format.pyi
+++ ruff_format.pyi
@@ -1,3 +1,13 @@
+def is_valid_syntax(code: str) -> bool:
+    """Check if the given Python code string has valid syntax.
+
+    Args:
+        code: The Python code to check.
+
+    Returns:
+        True if the code has valid syntax, False otherwise.
+    """
+
 def format_string(code: str) -> str:
     """Format a Python code string using Ruff.
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use pyo3::prelude::*;
 
 #[pyfunction]
+fn is_valid_syntax(code: &str) -> bool {
+    ruff_python_parser::parse_module(code).is_ok()
+}
+
+#[pyfunction]
 fn format_string(code: &str) -> PyResult<String> {
     ruff_python_formatter::format_module_source(
         code,
@@ -13,8 +18,9 @@ fn format_string(code: &str) -> PyResult<String> {
 }
 
 /// A Python module implemented in Rust.
-#[pymodule(gil_used=false)]
+#[pymodule(gil_used = false)]
 fn ruff_format(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(format_string, m)?)?;
+    m.add_function(wrap_pyfunction!(is_valid_syntax, m)?)?;
     Ok(())
 }
