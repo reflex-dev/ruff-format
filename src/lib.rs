2148a3b623f4734fc30252e73b1b611ba8ac639d
Khaleel Al-Adhami <khaleel.aladhami@gmail.com>
2026-03-20 13:57:00 -0700

    add parse_code and line_width

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,28 @@
 use pyo3::prelude::*;
 
 #[pyfunction]
+fn parse_code(code: &str) -> PyResult<()> {
+    ruff_python_parser::parse_module(code)
+        .map(|_| ())
+        .map_err(|e| {
+            PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!(
+                "Failed to parse code: {}",
+                e
+            ))
+        })
+}
+
+#[pyfunction]
 fn is_valid_syntax(code: &str) -> bool {
     ruff_python_parser::parse_module(code).is_ok()
 }
 
 #[pyfunction]
-fn format_string(code: &str) -> PyResult<String> {
+#[pyo3(signature = (code, *, line_width = 80u16))]
+fn format_string(code: &str, line_width: u16) -> PyResult<String> {
     ruff_python_formatter::format_module_source(
         code,
-        ruff_python_formatter::PyFormatOptions::default(),
+        ruff_python_formatter::PyFormatOptions::default().with_line_width(line_width.try_into()?),
     )
     .map_err(|e| {
         PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("Failed to format code: {}", e))
@@ -17,10 +30,10 @@ fn format_string(code: &str) -> PyResult<String> {
     .map(|formatted| formatted.into_code())
 }
 
-/// A Python module implemented in Rust.
 #[pymodule(gil_used = false)]
 fn ruff_format(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(format_string, m)?)?;
     m.add_function(wrap_pyfunction!(is_valid_syntax, m)?)?;
+    m.add_function(wrap_pyfunction!(parse_code, m)?)?;
     Ok(())
 }
